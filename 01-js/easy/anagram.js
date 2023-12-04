d58e31530bf0ca6e4d585164da7342a9182732e9
E Srinibash <93508152+E-srinibash@users.noreply.github.com>
2023-12-04 18:20:33 +0530

    Update anagram.js
    wrote the code for Is anagram function

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,10 @@
 */
 
 function isAnagram(str1, str2) {
+  str1=str1.replace(" ","").lower();
+  str2=str2.replace(" ","").lower();
 
+  return sorted(str1) === sorted(str2);
 }
 
 module.exports = isAnagram;

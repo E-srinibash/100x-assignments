a652e74292b1e1ef0258168afd2dcab01bf3688a
E-srinibash <ellasrinibash0@gmail.com>
2023-12-04 22:50:25 +0530

    Modified times.js file

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,18 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+      
+    let sum=0;
+
+    let now1 = new Date();
+    time1 = Math.floor(now1.getTime()/1000);
+
+    for(let i=1;i<=n;i++)
+        sum=sum+i;
+
+    let now2 = new Date();
+    time2 = Math.floor(now2.getTime()/1000);
+  
+    return time2-time1 ;
 }
+

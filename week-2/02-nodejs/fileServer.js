178ad046cc29fc1472d670295aa659d5e67cbba7
E-srinibash <ellasrinibash0@gmail.com>
2023-12-20 19:05:00 +0530

    wrote code for fileServer.js

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,38 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+
 const app = express();
 
+app.get("/files",(req,res)=>{
+  const directoryPath = "./files/";
+  fs.readdir(directoryPath,((err,file)=>{
+    if(err)
+    res.status(500).send("error reading files")
+    else
+    res.status(200).json({file});
+  })
+  )
+})
+
+app.get("/files/:filename",(req,res)=>{
+  const filename = req.params.filename;
+  const filePath = path.join("./files/",filename)
+  fs.readFile(filePath,"utf-8",(err,data)=>{
+    if(err)
+    res.status(404).send(err);
+    else
+    res.status(200).send(data);
+  })
+})
+
+const PORT = 3000
+app.listen(PORT,()=>{
+  console.log(`Server is running at ${PORT}`)
+});
 
 module.exports = app;
